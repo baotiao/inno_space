4e3ce3e0ba0e64fbf07a6edb9a842b20fd32ba28
baotiao <baotiao@gmail.com>
2021-12-13 21:17:45 +0800

    only show record information when page type is index_page

--- src/inno_space.cc
+++ src/inno_space.cc
@@ -105,7 +105,10 @@ void ShowIndexHeader(uint32_t page_num) {
   printf("Page level: %hu\n", mach_read_from_2(read_buf + PAGE_HEADER + PAGE_LEVEL));
   printf("Index ID: %lu\n", mach_read_from_8(read_buf + PAGE_HEADER + PAGE_INDEX_ID));
 
-  int rec_num = 0;
+  uint16_t page_type = mach_read_from_2(read_buf + FIL_PAGE_TYPE);
+  if (page_type != FIL_PAGE_INDEX) {
+    return;
+  }
   
   byte *rec_ptr = read_buf + PAGE_NEW_INFIMUM;
   printf("infimum %d\n", PAGE_NEW_INFIMUM);
